c34571a8ff8d1e52425a82aa5102a0d91c4985d8
ana-str <anastrugar20@gmail.com>
2022-01-03 01:31:54 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+package uni;
+
+import uni.controller.CourseController;
+import uni.controller.RegistrationSystem;
+import uni.controller.StudentController;
+import uni.controller.TeacherController;
+
+import uni.entities.Course;
+import uni.repository.*;
+import uni.view.ConsoleMenu;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        CourseJdbcRepository courseRepository = new CourseJdbcRepository();
+        CourseController courseController = new CourseController(courseRepository);
+
+        StudentJdbcRepository studentRepository = new StudentJdbcRepository();
+        StudentController studentController = new StudentController(studentRepository);
+
+        TeacherJdbcRepository teacherRepository = new TeacherJdbcRepository();
+        TeacherController teacherController = new TeacherController(teacherRepository);
+
+        RegistrationSystem registrationSystem = new RegistrationSystem(studentController, courseController, teacherController);
+        ConsoleMenu consoleMenu = new ConsoleMenu(registrationSystem);
+        consoleMenu.startConsole();
+}
